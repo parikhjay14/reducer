630aa64187ebb070d607660757ce7dc08409e985
Jay Parikh <jay@parikh.com>
2021-10-29 17:29:08 +0530

    jay parikh react native

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import ListViewScreen from './components/ListViewScreen';
+
+class App extends Component{
+    render() {
+        return <ListViewScreen />;
+    }
+}
+
+export default App;
